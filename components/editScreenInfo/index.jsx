1603ec98f3227f09a9d003b482f0f0c6c4f96066
valery99 <ver0003335>
2021-03-27 01:12:01 +0300

    deleted compiled js  files

--- components/editScreenInfo/index.jsx
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EditScreenInfo_1 = require("./EditScreenInfo");
-exports.default = EditScreenInfo_1.default;
--- screens/todoListScreen/index.jsx
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TodoListScreen_1 = require("./TodoListScreen");
-exports.default = TodoListScreen_1.default;
